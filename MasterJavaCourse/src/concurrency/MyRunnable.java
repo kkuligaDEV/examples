6e7e0bc33f5b814ea28cdc4ff4f03a0fb9343f8a
Vander <Chirs>
2018-05-28 18:55:04 +0200

    implemented example with Runnable interface implementation

--- MasterJavaCourse/src/concurrency/MyRunnable.java
+++ MasterJavaCourse/src/concurrency/MyRunnable.java
@@ -1,5 +1,25 @@
 package concurrency;
 
-public class MyRunnable {
+public class MyRunnable implements Runnable {
+
+	private int id;
+	private Thread thread = new Thread(this);
+	
+	public MyRunnable(int id) {
+		this.id = id;
+	}
+	public void start() {
+		thread.start();
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Hello from the "+this);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("MyRunnable{id=%d}", id);
+	}
 
 }
